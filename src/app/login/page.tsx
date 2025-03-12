0bfe5ee7f0ad13f52829cfb667435619fcd300bd
maisylattanavong <maisyltnv@kolaogroup.com>
2025-03-12 14:11:56 +0700

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,10 +4,12 @@ import { LoginForm } from "@/components/loginForm";
 
 export default function LoginPage() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-white" suppressContentEditableWarning>
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8">
+            <h1 className="text-4xl font-bold">Login</h1>
             <div className="w-full max-w-sm md:max-w-3xl ">
                 <LoginForm />
             </div>
         </div>
+
     )
 }
