5ccb75058a2198164ab20e632fecedff0a1c53c3
maisylattanavong <maisyltnv@kolaogroup.com>
2025-03-20 15:17:19 +0700

    update

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='p-4'>
+      <h1 className="text-3xl font-bold text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default HomePage
